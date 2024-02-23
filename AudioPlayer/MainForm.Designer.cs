105f4a9f18ccf2cb75c56e89c2571129d9900344
Alex.S <alexfordnir@gmail.com>
2024-02-23 19:32:27 +0200

    Добавьте файлы проекта.

---
+++ AudioPlayer/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace AudioPlayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Text = "Form1";
+            //
+            // TrackList
+            //
+            this.TrackList = new ListBox();
+            this.TrackList.Location = new Point(300, 0);
+            this.TrackList.Size = new Size(200, 400);
+            this.TrackList.ScrollAlwaysVisible = true;
+            //
+            // AddBtn
+            //
+            this.AddBtn = new Button();
+            this.AddBtn.Location = new Point(300, 400);
+            this.AddBtn.Size = new Size(100, 50);
+            this.AddBtn.Text = "ADD";
+            this.AddBtn.Click += AddClick;
+            //
+            // RemoveBtn
+            //
+            this.RemoveBtn = new Button();
+            this.RemoveBtn.Location = new Point(400, 400);
+            this.RemoveBtn.Size = new Size(100, 50);
+            this.RemoveBtn.Text = "Remove";
+            this.RemoveBtn.Click += RemoveClick;
+            //
+            // PrevBtn
+            //
+            this.PrevBtn = new Button();
+            this.PrevBtn.Location = new Point(0 , 400);
+            this.PrevBtn.Text = "<";
+            this.PrevBtn.Size = new Size(60, 50);
+            this.PrevBtn.Click += PrevClick;
+            //
+            // PlayBtn
+            //
+            this.PlayBtn = new Button();
+            this.PlayBtn.Location = new Point(60 , 400);
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.Size = new Size(60, 50);
+            this.PlayBtn.Click += PlayClick;           
+            //
+            // PauseBtn
+            //
+            this.PauseBtn = new Button();
+            this.PauseBtn.Location = new Point(120 , 400);
+            this.PauseBtn.Text = "Pause";
+            this.PauseBtn.Size = new Size(60, 50);
+            this.PauseBtn.Enabled = false;
+            this.PauseBtn.Click += PauseClick;
+            //
+            // StopBtn
+            //
+            this.StopBtn = new Button();
+            this.StopBtn.Location = new Point(180 , 400);
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.Size = new Size(60, 50);
+            this.StopBtn.Enabled = false;
+            this.StopBtn.Click += StopClick;
+            //
+            // NextBtn
+            //
+            this.NextBtn = new Button();
+            this.NextBtn.Location = new Point(240 , 400);
+            this.NextBtn.Text = ">";
+            this.NextBtn.Size = new Size(60, 50);
+            this.NextBtn.Click += NextClick;
+            //
+            // RandomBtn
+            //
+            this.RandomBtn = new Button();
+            this.RandomBtn.Location = new Point(0 , 0);
+            this.RandomBtn.Text = "Random Song";
+            this.RandomBtn.Size = new Size(150, 50);
+            this.RandomBtn.Click += RandomClick;
+            //
+            // ProgressBar
+            //
+            this.SongDuration = new TrackBar();
+            this.SongDuration.Location = new Point(0, 350);
+            this.SongDuration.Size = new Size(300, 10);
+            this.SongDuration.MouseDown += MouseDownEvent;
+            this.SongDuration.MouseUp += MouseUpEvent;
+            
+            //
+            // SoundPlayer
+            //
+            this.Sound = new SoundPlayer();
+
+            //
+            // Controls
+            //
+            this.Controls.Add(SongDuration);
+            this.Controls.Add(TrackList);
+            this.Controls.Add(AddBtn);
+            this.Controls.Add(RemoveBtn);
+            this.Controls.Add(PrevBtn);
+            this.Controls.Add(PlayBtn);
+            this.Controls.Add(PauseBtn);
+            this.Controls.Add(StopBtn);
+            this.Controls.Add(NextBtn);
+            this.Controls.Add(RandomBtn);
+        }
+
+      
+
+        SoundPlayer Sound;
+        ListBox TrackList;
+        Button AddBtn, RemoveBtn , PrevBtn , PlayBtn , PauseBtn, StopBtn , NextBtn , RandomBtn;
+        TrackBar SongDuration;
+         
+
+        #endregion
+    }
+}
+
---
+++ AudioPlayer/MainForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Wave;
+
+namespace AudioPlayer
+{
+    public partial class MainForm : Form
+    {
+        IWavePlayer Wave;
+        AudioFileReader Reader;
+        Dictionary<string, string> TrackDictionary;
+        Timer t;
+        bool OnTrackBar;
+        public MainForm()
+        {
+            InitializeComponent();
+            Wave = new WaveOut();
+            TrackDictionary = new Dictionary<string, string>();
+            t = new Timer();
+            t.Tick += TimerTick;
+            t.Interval = 100;
+            
+
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (OnTrackBar == true)
+            {
+                Reader.CurrentTime = new TimeSpan(0, 0, SongDuration.Value);
+            }
+            else if(OnTrackBar == false)
+            {
+                SongDuration.Value = Convert.ToInt32(Reader.CurrentTime.Seconds);
+            }
+        }
+        private void PlayClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = TrackDictionary.Values.ElementAt(TrackList.SelectedIndex);
+                Reader = new AudioFileReader(s);
+                Wave.Init(Reader);
+                PauseBtn.Enabled = true;
+                StopBtn.Enabled = true;
+                PlayBtn.Enabled = false;
+                Wave.Play();
+                t.Start();
+                SongDuration.Maximum = Convert.ToInt32(Reader.TotalTime.TotalSeconds);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Трэк не выбран или не соответсвует формату(mp3 , wav)");
+            }
+
+        }
+        private void PauseClick(object sender, EventArgs e)
+        {
+            Wave.Pause();
+            PauseBtn.Enabled = false;
+            StopBtn.Enabled = false;
+            PlayBtn.Enabled = true;
+        }
+        private void StopClick(object sender, EventArgs e)
+        {
+            Wave.Stop();
+            if (Wave != null) Wave.Dispose();
+            PauseBtn.Enabled = false;
+            StopBtn.Enabled = false;
+            PlayBtn.Enabled = true;
+        }
+        private void PrevClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TrackList.Items.Count == 0)
+                {
+                    throw new InvalidOperationException();
+                }
+                if (TrackList.SelectedIndex - 1 < 0)
+                {
+                    TrackList.SelectedIndex = TrackList.Items.Count - 1;
+                }
+                else
+                {
+                    TrackList.SelectedIndex = TrackList.SelectedIndex - 1;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Трэк-Лист пуст");
+            }
+        }
+        private void NextClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TrackList.SelectedIndex + 1 > TrackList.Items.Count - 1)
+                {
+                    TrackList.SelectedIndex = 0;
+                }
+                else
+                {
+                    TrackList.SelectedIndex = TrackList.SelectedIndex + 1;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Трэк-Лист пуст");
+            }
+        }
+        private void AddClick(object sender, EventArgs e)
+        {
+            OpenFileDialog Open = new OpenFileDialog();
+
+            Open.Filter = "WAV (*.wav)|*.wav|MP3 (*.mp3)|*.mp3|Flac (*.flac)|*.flac";
+            Open.FilterIndex = 2;
+            Open.RestoreDirectory = true;
+
+            string FilePath;
+
+            if (Open.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = Open.FileName;
+                DirectoryInfo info = new DirectoryInfo(FilePath);
+                try
+                {
+                    TrackDictionary.Add(info.Name, FilePath);
+                    TrackList.Items.Add(TrackDictionary.Keys.ElementAt(TrackDictionary.Count - 1));
+                }
+                catch
+                {
+                    MessageBox.Show($"Песня с названием {info.Name} уже была добавлена ранее");
+                }
+
+            }
+            TrackList.SelectedIndex = 0;
+        }
+        private void RemoveClick(object sender, EventArgs e)
+        {
+            TrackList.Items.RemoveAt(TrackList.SelectedIndex);
+        }
+        private void RandomClick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int Num = rand.Next(0, TrackList.Items.Count);
+            if (Num == TrackList.SelectedIndex)
+            {
+                Num = rand.Next(0, TrackList.Items.Count);
+            }
+            if (TrackList.Items.Count != 0)
+            {
+                TrackList.SelectedIndex = Num;
+            }
+            else
+            {
+                MessageBox.Show("Трэк-лист пуст");
+            }
+        }
+        private void MouseDownEvent(object sender, MouseEventArgs e)
+        {
+            OnTrackBar = true;
+        }
+        private void MouseUpEvent(object sender, MouseEventArgs e)
+        {
+            OnTrackBar = false;
+        }
+    }
+}
